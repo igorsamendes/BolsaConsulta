60be6bbebe64aec181819c0aa0e80fc97f255138
Igor Mendes <igormendes.ufop@gmail.com>
2024-06-07 00:23:23 -0300

    ajuste

--- BolsaConsultaServer/src/server.js
+++ BolsaConsultaServer/src/server.js
@@ -33,7 +33,7 @@ app.post('/api/logs', async (req, res) => {
                 qtd_beneficiados: dados.qtd_beneficiados
             }
         });
-        res.status(201).json({ message: "Dados inseridos com sucesso!", dados });
+        res.status(201).json({ message: "Dados inseridos com sucesso!" });
     } catch (error) {
         console.error("Erro ao inserir dados:", error);
         res.status(500).json({ error: "Erro ao inserir dados" });
