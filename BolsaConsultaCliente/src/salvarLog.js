6e82d2080dd9c8ec0d6613ffbaf5689619edfd1d
Igor Mendes <igormendes.ufop@gmail.com>
2024-06-07 01:19:15 -0300

    testes

--- BolsaConsultaCliente/src/salvarLog.js
+++ BolsaConsultaCliente/src/salvarLog.js
@@ -1,10 +1,10 @@
 // Termina a construção do array que contem os dados de log e faz a chamada da rota de salvamento dos dados em banco
 
-import axios from 'axios';
 const salvarLog = async (dadosConsulta) => {
   try {
-    const response = await axios.get('https://api64.ipify.org?format=json');
-    const ipAddress = response.data.ip;
+    const response = await fetch('https://api64.ipify.org/?format=json');
+    const data = await response.json();
+    const ipAddress = data.ip;
 
     const dadosCompletos = {
       ip: ipAddress,
