d2ba6b117e1c80fe08170e562b63f6510e18b06d
Igor Mendes <igormendes.ufop@gmail.com>
2024-06-08 10:24:06 -0300

    Criando endopoint de get

--- BolsaConsultaServer/src/server.js
+++ BolsaConsultaServer/src/server.js
@@ -7,15 +7,6 @@ const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "" indicando que qualquer site pode fazer a conexão
-//     res.header("Access-Control-Allow-Origin", "");
-//     //Quais são os métodos que a conexão pode realizar na API
-//     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-//     app.use(cors());
-//     next();
-// });
-
 app.use(cors({
     origin: 'https://bolsa-consulta.vercel.app', // Permitir requisições deste domínio
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -25,6 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
+// Endpoint para criar um novo log
 app.post('/api/logs', async (req, res) => {
     const dados = req.body;
 
@@ -40,10 +32,23 @@ app.post('/api/logs', async (req, res) => {
                 qtd_beneficiados: dados.qtd_beneficiados
             }
         });
-        res.status(200);
+        res.status(200).json({ message: 'Log salvo com sucesso' });
     } catch (error) {
         console.error("Erro ao inserir dados:", error);
         res.status(500).json({ error: "Erro ao inserir dados" });
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
+// Endpoint para recuperar todos os logs
+app.get('/api/logs', async (req, res) => {
+    try {
+        const logs = await prisma.log_consulta.findMany();
+        res.status(200).json(logs);
+    } catch (error) {
+        console.error("Erro ao recuperar dados:", error);
+        res.status(500).json({ error: "Erro ao recuperar dados" });
     } finally {
         await prisma.$disconnect();
     }
